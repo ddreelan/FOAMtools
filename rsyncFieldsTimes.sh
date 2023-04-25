d15de8e19c39767f49e719067cfafb74e62b13bb
ddreelan <80991602+ddreelan@users.noreply.github.com>
2023-04-25 15:42:21 +0100

    Add files via upload

---
+++ rsyncFieldsTimes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Declaring bash functions"
+rsyncFieldsTimes () {
+  echo "arg1 is the source case, full of time directories"
+  echo "arg2 is the target case name"
+  echo "arg3 is the file of times to be mapped"
+  echo "arg4 is the file of fields to be mapped"
+
+  echo "Copying constant"
+  rsync -azvhm --include="constant/" --include="processor*/constant" --exclude="*"  "${1}/" $2/
+
+  echo "Copy system"
+  rsync -vazhm $1/system/* $2/system/
+
+  while read t; do
+    echo  "Rsyncing time ${t}" 
+    while read f; do
+    echo "Copying field ${f} for time ${t}"
+    rsync -azvhm --include="${t}/" --include="processor*/" --include="${f}" --exclude="*"  "${1}/" $2/
+    done <$4
+  done <$3
+
+  echo "Rsyncing complete"
+}
+
+echo "Loading of rsync function complete"
+
